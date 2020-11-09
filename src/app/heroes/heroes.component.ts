d444f56efb6583b8567b54c67cd8738f490c97da
Jérémy RATSIMANDRESY <jeremy.ratsimandresy@gmail.com>
2020-11-09 17:17:48 +0100

    Add in-app navigation with routing : router link, navigation, parameters in route, redirect

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,6 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
@@ -23,9 +22,4 @@ export class HeroesComponent implements OnInit {
     this.getHeroes()
   }
 
-
-  onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-      this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`)
-  }
 }
