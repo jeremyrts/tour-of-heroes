54e413a3b29175dc50c52f9e8789263ddcfdf125
Jérémy RATSIMANDRESY <jeremy.ratsimandresy@gmail.com>
2020-12-02 16:43:40 +0100

    More tests

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -2,22 +2,25 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HeroService } from '../hero.service';
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero'
 
 import { HeroesComponent } from './heroes.component';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
+  const testHeroes = HEROES
+  const heroToDelete = testHeroes[0]
+  const heroToAdd = "ShibaMan"
+  const heroesService = jasmine.createSpyObj('HeroService', ['getHeroes', 'deleteHero', 'addHero']);
+  const getHeroes = heroesService.getHeroes.and.returnValue(of(testHeroes));
 
 
   beforeEach(async () => {
-    const testHeroes = HEROES
-    const heroToDelete = testHeroes[0]
-    const heroesService = jasmine.createSpyObj('HeroService', ['getHeroes']);
-    const getHeroes = heroesService.getHeroes.and.returnValue(of(testHeroes));
+    
     await TestBed.configureTestingModule({
       declarations: [HeroesComponent],
       imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([])],
@@ -43,25 +46,37 @@ describe('HeroesComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should remove a hero method', () => {
-    fixture.detectChanges()
-    const spyDelete = spyOn(component, 'delete'); 
-    const listHeroes = fixture.debugElement.queryAll(By.css('li'));
+  it('should call delete() method and remove a hero', () => {
+    const numberOfHeroes = component.heroes.length
+    const spyDelete = spyOn(component, 'delete').and.callThrough(); 
+    heroesService.deleteHero.and.callFake(() => {
+      return of(heroToDelete);
+    })
     const deleteButton = fixture.debugElement.queryAll(By.css('.delete'))[0];
     deleteButton.triggerEventHandler('click', null)
-    // const newlistHeroes = fixture.debugElement.queryAll(By.css('li'));
+    fixture.detectChanges()
     expect(spyDelete).toHaveBeenCalledTimes(1)
-    // expect(newlistHeroes.length).toEqual(listHeroes.length - 1)
+    expect(component.heroes.length).toEqual(numberOfHeroes - 1)
   });
+ 
 
+  it('should call add() method and add a hero', () => {
 
-  it('should call add() method ', () => {
-
-    const spyAdd = spyOn(component, 'add');
+    const numberOfHeroes = component.heroes.length
     const compiled = fixture.nativeElement;
+    const input = compiled.querySelector('input')
+    input.value = heroToAdd
+    input.dispatchEvent(new Event('input'))
+
+    const spyAdd = spyOn(component, 'add').and.callThrough();
+
+    heroesService.addHero.and.callFake(() => {
+      return of(heroToAdd);}) // Just to pass the subscribe
+    
     const addButton = compiled.querySelector('button')
     addButton.click()
     expect(spyAdd).toHaveBeenCalledTimes(1)
+    expect(component.heroes.length).toEqual(numberOfHeroes + 1)
   })
 
 
