1170fa24eb2ca0e68bfd3f77192728c88611756a
Jérémy RATSIMANDRESY <jeremy.ratsimandresy@gmail.com>
2020-12-03 17:35:04 +0100

    Testing methods in HeroDetailComponent

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -3,15 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from '../hero.service';
+import { HEROES } from '../mock-heroes';
+import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  const testHero: Hero = HEROES[0]
+  const heroesService: jasmine.SpyObj<HeroService> = jasmine.createSpyObj('HeroService', ['getHero', 'deleteHero', 'addHero', 'updateHero']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HeroDetailComponent ],
-      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([])],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([]), FormsModule],
+      providers: [{provide: HeroService, useValue: heroesService}]
     })
     .compileComponents();
   });
@@ -19,10 +27,33 @@ describe('HeroDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    heroesService.getHero.and.callFake(() => {
+      return of(testHero)
+    })
+    heroesService.updateHero.and.callFake(() => {
+      return of(testHero)
+    })
+    fixture.detectChanges(); 
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component).toBeDefined()
   });
+
+  it('should call goBack()', () => {
+    const buttonBack = fixture.nativeElement.querySelectorAll('button')[0]
+    const spyGoBack = spyOn(component, 'goBack').and.callThrough()
+    buttonBack.click()
+    expect(spyGoBack).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call updateHero() from heroService', () => {
+    const compiled = fixture.nativeElement;
+    const buttonSave = compiled.querySelectorAll('button')[1]
+    const spyUpdate = spyOn(component, 'save').and.callThrough()
+    buttonSave.click()
+    expect(spyUpdate).toHaveBeenCalledTimes(1)
+  })
 });
